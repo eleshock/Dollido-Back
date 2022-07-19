1c4ed947cebe1613701d7030f9dd73ffaaad7c9a
Ubuntu <ubuntu@ip-172-31-41-151.ap-northeast-2.compute.internal>
2022-07-19 11:44:29 +0000

    Best Performer 구현 완(서버에서 POST로 영상 받고, GET으로 Best performer 영상 뿌려주는 것까지

--- server/server.js
+++ server/server.js
@@ -7,18 +7,23 @@ import webSocket from "./Routers/socket";
 import apiGifs from "./Routers/gifs";
 import apiMember from "./Routers/member";
 import apiTest from "./Routers/test";
+import apiBestPerformer from "./Routers/bestPerformer";
 
 const port = 5000;
 const app = express();
 const server = http.createServer(app);
 
 app.use(cors());
+app.use(express.json()); // 이거 넣어줘야 post에서 데이터 받을 수 있다규..ㅠ
 app.use(express.static('./server/public'));
+app.use(express.static('./server/bestVideos'));
+app.use(express.urlencoded({ extended: false })); // app을 통해 들어오는 모든 요청은 bodyParser라는 미들웨어를 거쳐 라우트로 전달
 
 // routers
 app.use("/api/test", apiTest);
 app.use("/api/gifs", apiGifs);
 app.use("/api/member", apiMember);
+app.use("/api/best", apiBestPerformer);
 
 // webSocket, https 연결 
 webSocket(server);
