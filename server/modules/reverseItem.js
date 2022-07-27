345fbb6112bdf077b99f1e33d64715c16495a9d3
Ubuntu <ubuntu@ip-172-31-45-219.ap-northeast-2.compute.internal>
2022-07-27 22:12:24 +0000

    28일 중간발표

--- server/modules/reverseItem.js
+++ server/modules/reverseItem.js
@@ -2,6 +2,9 @@
 const chooseReverseUser = (rooms, roomID) => {
     let untilMinHP = 10**6;
     let choosenUser = "anonymous";
+    if (!rooms[roomID]){
+        return ;
+    }
     for (const member of rooms[roomID].members) {
         if (member.HP < untilMinHP) {
             untilMinHP = member.HP;
