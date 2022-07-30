bbee836e8ac142c6caa0eb6864062c89ea8b9d76
Ubuntu <ubuntu@ip-172-31-45-219.ap-northeast-2.compute.internal>
2022-07-30 11:52:03 +0000

    sharp compress

--- server/s3.js
+++ server/s3.js
@@ -1,42 +1,50 @@
 require('dotenv').config()
 const fs = require('fs')
 const S3 = require('aws-sdk/clients/s3')
+// const sharp = require('sharp');
+
 import { v4 as uuidv4 } from 'uuid';
 
 
 
 const bucketName = process.env.AWS_BUCKET_NAME
-const region=process.env.AWS_BUCKET_REGION
-const accessKeyId=process.env.AWS_ACCESS_KEY
-const secretAccessKey=process.env.AWS_SECRET_KEY
+const region = process.env.AWS_BUCKET_REGION
+const accessKeyId = process.env.AWS_ACCESS_KEY
+const secretAccessKey = process.env.AWS_SECRET_KEY
 
 const s3 = new S3({
-    region,
-    accessKeyId,
-    secretAccessKey
+  region,
+  accessKeyId,
+  secretAccessKey
 })
 
 // uploads a file to s3
-function uploadFile(file) {
-    const fileStream = fs.createReadStream(file.path)
-    const uploadParams = {
-      Bucket: bucketName,
-      Body: fileStream,
-      Key: 'myWeapon/' + uuidv4() + file.originalname
-    }
-
-    return s3.upload(uploadParams).promise()
+function uploadFile(file, compressedFileStream, dirPath = "") {
+  console.log(file.size);
+  let fileStream;
+  if (!compressedFileStream) {
+    fileStream = fs.createReadStream(file.path)
+  } else {
+    fileStream = compressedFileStream;
   }
-  exports.uploadFile = uploadFile
+  const uploadParams = {
+    Bucket: bucketName,
+    Body: fileStream,
+    Key: dirPath + uuidv4() + file.originalname
+  }
+
+  return s3.upload(uploadParams).promise()
+}
+exports.uploadFile = uploadFile
 
 
 //   downloads a file from s3
-  function getFileStream(fileKey) {
-    const downloadParams = {
-      Key: fileKey,
-      Bucket: bucketName
-    }
-
-    return s3.getObject(downloadParams).createReadStream()
+function getFileStream(fileKey) {
+  const downloadParams = {
+    Key: fileKey,
+    Bucket: bucketName
   }
-  exports.getFileStream = getFileStream
+
+  return s3.getObject(downloadParams).createReadStream()
+}
+exports.getFileStream = getFileStream
