351ecf4d05704c344f9fee50039f5ab21ce338af
yunchanpark <yongp99@nate.com>
2022-08-02 17:22:48 +0000

    예외 처리

--- server/Routers/bestPerformer/bestPerformer.js
+++ server/Routers/bestPerformer/bestPerformer.js
@@ -30,108 +30,124 @@ function getNowTime() {
 
 /** 유저가 자신의 웃는 영상(videoFile)을 user_nick와 함께 보내면 이를 저장하고 bestVideoPath에 보관 */
 router.post('/send-video', upload.single("video"), authUtil, async (req, res) => {
-    const user_nick = req.body.user_nick;
-    const videoFile = req.file;
-    videoFile.originalname = `_${user_nick}.mp4`;
+    try {
+        const user_nick = req.body.user_nick;
+        const videoFile = req.file;
+        videoFile.originalname = `_${user_nick}.mp4`;
+        
+        const uploadRes = await uploadFile(videoFile, 'bestVideos/'); // Upload to S3
+        console.log("Upload Location :", uploadRes.Location);
+        
+        const videoPath = `${uploadPath}/${videoFile.filename}`;
+        fs.unlink(videoPath, (err) => {    // Delete local video file
+            if (err) console.log(err);
+        });
+        
+        const nowTime = getNowTime();
+        const newVideoName = `${user_nick}-${nowTime}.mp4`;
+        
+        // Insert Video Info into MySQL DB
+        const member_id = req.idx;
+        const args = [member_id, newVideoName, uploadRes.key];
+        if (!await queryGet(bestVideoQuery.insertVideo, args)) { // 실패하면
+            deleteObject(uploadRes.key);                         // s3에서 삭제
+        }
     
-    const uploadRes = await uploadFile(videoFile, 'bestVideos/'); // Upload to S3
-    console.log("Upload Location :", uploadRes.Location);
+        console.log(`${user_nick} video uploaded. size : ${videoFile.size}`);
     
-    const videoPath = `${uploadPath}/${videoFile.filename}`;
-    fs.unlink(videoPath, (err) => {    // Delete local video file
-        if (err) console.log(err);
-    });
+        bestVideos[user_nick] = uploadRes.key;    // bestVideos에 등록
     
-    const nowTime = getNowTime();
-    const newVideoName = `${user_nick}-${nowTime}.mp4`;
-    
-    // Insert Video Info into MySQL DB
-    const member_id = req.idx;
-    const args = [member_id, newVideoName, uploadRes.key];
-    if (!await queryGet(bestVideoQuery.insertVideo, args)) { // 실패하면
-        deleteObject(uploadRes.key);                         // s3에서 삭제
+        res.status(201).send({ success: true, msg: "파일이 성공적으로 전송되었습니다." });
+    } catch (e) {
+        res.status(400).send({success: false, msg: "파일 업로드가 실패했습니다."})
     }
-
-    console.log(`${user_nick} video uploaded. size : ${videoFile.size}`);
-
-    bestVideos[user_nick] = uploadRes.key;    // bestVideos에 등록
-
-    res.status(201).send({ success: true, msg: "파일이 성공적으로 전송되었습니다." });
 });
 
 
 /** best performer의 id와 비디오 이름 던져줌 */
 router.post("/get-best", async (req, res) => {
-    const room = rooms[req.body.roomID];
-    let member_id = 0;
-
-    if (room === undefined) {
-        res.status(404).send({ msg: `존재하지 않는 방입니다. roomID : ${req.body.roomID}` });
-        return;
-    }
-
-    const userNick = req.body.user_nick
-    console.log(userNick)
-    let member = await queryGet(memberQuery.findMemberIdByNickname, userNick)
-    console.log(member)
-    member_id = member[0].member_id
-    let result = await queryGet(changableFactorsQuery.getCF, member_id)
-
-    if (!result[0]) {
-        await queryGet(changableFactorsQuery.insertCF, [member_id, 0, 0, 0, 0, ""])
-        result = await queryGet(changableFactorsQuery.getCF, member_id)
-    }
+    try {
+        const room = rooms[req.body.roomID];
+        let member_id = 0;
     
-    let [{ point, win, lose, ranking, tier }] = result;
-    let [hey] = await queryGet(changableFactorsQuery.rank, member_id);
-
-    const bestPerformerNick = room.bestPerformer;
-    if (bestPerformerNick == null) {
-        res.status(404).send({ msg: "best performer가 지정되지 않았습니다." });
-    } else {
-        if (bestPerformerNick === userNick) lose += 1
-        else win += 1
-
-        point = Math.round(win / (lose + win) * 100 * 100) / 100;
-        let countUser = await queryGet(changableFactorsQuery.findCnt)
-        let myRankByCount = hey.rank / countUser[0].cnt
-
-        if (myRankByCount <= 0.2) tier = '모아이'
-        else if (0.2 < myRankByCount <= 0.4) tier = '가오나시'
-        else if (0.4 < myRankByCount <= 0.7) tier = '모나리자'
-        else tier = '하회탈'
-
-        await queryGet(changableFactorsQuery.updateCFById, [point, win, lose, hey.rank, tier, member_id])
-
-        const bestVideoName = bestVideos[bestPerformerNick];
-        console.log('Best Video Name :', bestVideoName);
-        res.send({ bestPerformerNick: bestPerformerNick, bestVideoName: bestVideoName });
+        if (room === undefined) {
+            res.status(404).send({ msg: `존재하지 않는 방입니다. roomID : ${req.body.roomID}` });
+            return;
+        }
+    
+        const userNick = req.body.user_nick
+        console.log(userNick)
+        let member = await queryGet(memberQuery.findMemberIdByNickname, userNick)
+        console.log(member)
+        member_id = member[0].member_id
+        let result = await queryGet(changableFactorsQuery.getCF, member_id)
+    
+        if (!result[0]) {
+            await queryGet(changableFactorsQuery.insertCF, [member_id, 0, 0, 0, 0, ""])
+            result = await queryGet(changableFactorsQuery.getCF, member_id)
+        }
+        
+        let [{ point, win, lose, ranking, tier }] = result;
+        let [hey] = await queryGet(changableFactorsQuery.rank, member_id);
+    
+        const bestPerformerNick = room.bestPerformer;
+        if (bestPerformerNick == null) {
+            res.status(404).send({ msg: "best performer가 지정되지 않았습니다." });
+        } else {
+            if (bestPerformerNick === userNick) lose += 1
+            else win += 1
+    
+            point = Math.round(win / (lose + win) * 100 * 100) / 100;
+            let countUser = await queryGet(changableFactorsQuery.findCnt)
+            let myRankByCount = hey.rank / countUser[0].cnt
+    
+            if (myRankByCount <= 0.2) tier = '모아이'
+            else if (0.2 < myRankByCount <= 0.4) tier = '가오나시'
+            else if (0.4 < myRankByCount <= 0.7) tier = '모나리자'
+            else tier = '하회탈'
+    
+            await queryGet(changableFactorsQuery.updateCFById, [point, win, lose, hey.rank, tier, member_id])
+    
+            const bestVideoName = bestVideos[bestPerformerNick];
+            console.log('Best Video Name :', bestVideoName);
+            res.send({ bestPerformerNick: bestPerformerNick, bestVideoName: bestVideoName });
+        }
+    } catch (e) {
+        console.log(e);
     }
 });
 
 
 router.get("/my-videos", authUtil, async (req, res) => {
-    const member_id = req.idx;
-    
-    const result = await queryGet(bestVideoQuery.getMyVideos, [member_id]);
-    if (result) res.send(result);
-    else res.send({msg : "DB에서 데이터를 가져오는 데 실패했습니다."})
+    try {
+        const member_id = req.idx;
+        
+        const result = await queryGet(bestVideoQuery.getMyVideos, [member_id]);
+        if (result) res.send(result);
+        else res.send({msg : "DB에서 데이터를 가져오는 데 실패했습니다."})
+    } catch (e) {
+        console.log(e);
+    }
 })
 
 
 router.post("/delete-video", authUtil, async (req, res) => {
-    const video_id = req.body.video_id;
-    const server_name = req.body.server_name;
-
-    const dbDeletionRes = await queryGet(bestVideoQuery.deleteVideo, [video_id])
-    if (!dbDeletionRes) { // DB deletion fail
-        res.status(400).send({msg: "DB 삭제 실패"});
-        return;
-    }
-
-    await deleteObject(server_name);
+    try {
+        const video_id = req.body.video_id;
+        const server_name = req.body.server_name;
     
-    res.send({ msg : "삭제 성공 "});
+        const dbDeletionRes = await queryGet(bestVideoQuery.deleteVideo, [video_id])
+        if (!dbDeletionRes) { // DB deletion fail
+            res.status(400).send({msg: "DB 삭제 실패"});
+            return;
+        }
+    
+        await deleteObject(server_name);
+        
+        res.send({ msg : "삭제 성공 "});
+    } catch(e) {
+        console.log(e);
+    }
 })
 
 module.exports = router;
